c730a4c7d85603fbedf309732809a2ce535769c8
persepolis1 <electry.ba@gmail.com>
2016-08-19 21:53:49 -0400

    Initial App Structure

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,16 @@
+/**
+ * Created by bobak on 2016-08-18.
+ */
+import React from 'react';
+
+class AboutPage extends React.Component{
+  render(){
+    return(
+      <div>
+        <h1>About</h1>
+        <p>This app uses React, Redux and other libs</p>
+      </div>
+    );
+  }
+}
+export default AboutPage;
---
+++ src/components/common/header.js
@@ -0,0 +1,18 @@
+/**
+ * Created by bobak on 2016-08-19.
+ */
+import React, {PropTypes} from 'react';
+import {Link, IndexLink} from 'react-router';
+
+const  Header = () => {
+  return(
+    <nav>
+      <IndexLink to="/" activeClassName="active">Home</IndexLink>
+      {" | "}
+      <Link to="/courses" activeClassName="active">Courses</Link>
+      {" | "}
+      <Link to="/about" activeClassName="active">About</Link>
+    </nav>
+  );
+};
+export default Header;
